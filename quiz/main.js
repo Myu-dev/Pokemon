28dda5dbffa9a35c45cdacc5853193ca487373f6
Myu <myu102009@gmail.com>
2026-05-05 00:22:14 +0900

    ヒントの表示を変更

--- quiz/main.js
+++ quiz/main.js
@@ -1,5 +1,14 @@
-const startBuutton=document.getElementById("start");
+const card=document.getElementById("mainContainer");
+const startBuutton=card.getElementById("start");
 startBuutton.addEventListener("click",async()=>{
     const pokemon=await Pokemon.getPokemonRandom(1,152);
     console.log(pokemon.data);
+    card.innerHTML=`
+    <h2>ポケモンクイズ</h2>
+    <div id="hint" class="hint">
+    図鑑番号: No.${pokemon.data.id}
+    <div>
+    <input placeholder="ポケモン名を入力"></input>
+    <button>答える<button>
+    `
 })
