2297ae3ac745791c17ea4bffc714065024366a99
Myu <myu102009@gmail.com>
2026-05-05 09:28:02 +0900

    ヒントの代入の場所を変更

--- quiz/main.js
+++ quiz/main.js
@@ -1,3 +1,5 @@
+let hints;
+
 const card=document.getElementById("mainContainer");
 const startBuutton=document.getElementById("start");
 startBuutton.addEventListener("click",async()=>{
@@ -10,17 +12,16 @@ startBuutton.addEventListener("click",async()=>{
     </div>
     <input id="guess" placeholder="ポケモン名を入力"></input>
     <button id="guess-button">答える</button>
-    `
+    `;
+    hints=[
+        `<p>No.${pokemon.data.id}</p>`,
+        `<p>${pokemon.data.genera.ja}</p>`,
+        `<p>${pokemon.data.height/10}m ${pokemon.data.weight/10}kg</p>`,
+    ];
 });
 const hintEl=document.getElementById("hint");
 const guess=document.getElementById("guess");
 const guessButton=document.getElementById("guess-button");
-const hints=[
-    `<p>No.${pokemon.data.id}</p>`,
-    `<p>${pokemon.data.genera.ja}</p>`,
-    `<p>${pokemon.data.height/10}m ${pokemon.data.weight/10}kg</p>`,
-
-];
 const showHints=["0","02","012"];
 let count=0;
 guessButton.addEventListener("click",()=>{
